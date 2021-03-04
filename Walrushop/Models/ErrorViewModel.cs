2cf82e06a8c9f49fc6ae0bf8c2b394e6f6756d24
abitj <https://github.com/abitj>
2021-03-04 12:32:28 -0600

    Built Walrushop Site

---
+++ Walrushop/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Walrushop.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
